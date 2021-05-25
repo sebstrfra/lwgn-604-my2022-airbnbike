e5d85d6cbc5de3bdea76e0b5b7cef423870739c1
Jonas Hagedorn <hagedorn.j@gmx.de>
2021-05-25 23:46:28 +0200

    save changes and restart pc

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -7,4 +7,23 @@ class BikesController < ApplicationController
   def index
     @bikes = Bike.all
   end
+
+  def new
+    @bike = Bike.new
+  end
+
+  def create
+    @bike = Bike.new(bike_params)
+    @bike.user_id = current_user.id
+
+    if @bike.save
+      redirect_to bike_path(@bike)
+    else
+      render :new
+    end
+  end
+
+  def bike_params
+    params.require(:bike).permit(:name, :description, :category, :location, photos: [])
+  end
 end
