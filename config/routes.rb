675ed96b62d08c776a5920d45a598eb54ee518dc
Sebastian Strube <sebstrfra@gmail.com>
2021-05-27 12:24:03 +0200

    Dashbboard Method in Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :bikes, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :bikes
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
+
 end
